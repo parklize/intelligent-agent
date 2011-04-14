bc8f5239d7a4082eacbcc5006e62b27e12632576
parklize <parklize@906de103-2642-d4b0-453b-d9ae781199c4>
2011-04-14 06:13:04 +0000

    swcl qualifier is added without binding class info

--- trunk/OWL API Test/src/com/park/swcl/examples/Create_SWCL_Example01.java
+++ trunk/OWL API Test/src/com/park/swcl/examples/Create_SWCL_Example01.java
@@ -6,7 +6,10 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
@@ -34,19 +37,36 @@ public class Create_SWCL_Example01 {
 			PrefixManager pm = new DefaultPrefixManager(base);
 			
 			// get the reference to the Constraint class
-			OWLClass constraint = dataFactory.getOWLClass(":Constraint",pm);
+			OWLClass constraint = dataFactory.getOWLClass("#Constraint",pm);
+			
+			// get the reference to the Variable class(create)
+			OWLClass variable = dataFactory.getOWLClass("#Variable",pm);
 			
 			// get the reference to the numberOfPopulation instance
-			OWLNamedIndividual numberOfPopulation = dataFactory.getOWLNamedIndividual(":numberOfPopulation",pm);
+			OWLNamedIndividual numberOfPopulation = dataFactory.getOWLNamedIndividual("#numberOfPopulation",pm);
+			
+			// get the reference to the y instance 
+			OWLIndividual y = dataFactory.getOWLNamedIndividual("#y",pm);
 			
 			// create class assertion that numberOfPopulation is the instance of constraint class
 			OWLClassAssertionAxiom classAssertion = dataFactory.getOWLClassAssertionAxiom(constraint, numberOfPopulation);
+			
+			// create class assertion that y is the instance of the Variable
+			OWLClassAssertionAxiom yClassAssertion = dataFactory.getOWLClassAssertionAxiom(variable, y);
+			
+			// create Object property named qualifier
+			OWLObjectProperty qualifier = dataFactory.getOWLObjectProperty("#qualifier",pm);
+			
+			// create Object property assertion that numberOfPopulation qualifier y
+			OWLObjectPropertyAssertionAxiom qualifierObjectPropertyAssertion = dataFactory.getOWLObjectPropertyAssertionAxiom(qualifier, numberOfPopulation, y);
 			
 			// create an ont to add the assertion
 			OWLOntology ontology = manager.createOntology(IRI.create(base));
 				
 			// add the class assertion to an ontoloty
 			manager.addAxiom(ontology,classAssertion);
+			manager.addAxiom(ontology, yClassAssertion);
+			manager.addAxiom(ontology, qualifierObjectPropertyAssertion);
 			
 			
 			// dump the ontology to the sysout
